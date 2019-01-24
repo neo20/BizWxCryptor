a99a44b63c5e9300272044146570ef63279df29f
Neo20 <350176528@qq.com>
2019-01-24 08:45:55 +0800

    init

---
+++ BizWxUtil.py
@@ -0,0 +1,68 @@
+from django.http import HttpResponse
+from .WXBizMsgCrypt import WXBizMsgCrypt,ResponseMessage,generateNonce
+import xml.etree.cElementTree as ET
+
+def getCryptor():
+    # TODO hard code configuration
+    sToken = "edit_this"
+    sEncodingAESKey = "edit_this"
+    sCorpID = "edit_this"
+
+    return WXBizMsgCrypt(sToken,sEncodingAESKey,sCorpID)
+
+def responseEcho(request):
+
+    sVerifyMsgSig = request.GET.get('msg_signature')
+    sVerifyNonce = request.GET.get('nonce')
+    sVerifyTimeStamp = request.GET.get('timestamp')
+    sVerifyEchoStr = request.GET.get('echostr')
+    
+    wxcpt = getCryptor()
+
+    ret,sEchoStr=wxcpt.VerifyURL(sVerifyMsgSig, sVerifyTimeStamp,sVerifyNonce,sVerifyEchoStr)
+    if ret != 0 :
+        print("ERR: VerifyURL ret: " + str(ret))
+        return HttpResponse("ERR: VerifyURL ret: " + str(ret))
+
+    return HttpResponse(sEchoStr)
+
+# decrypt message and ruturn as python dict
+def parseMessage(request):
+    encrypted_xml = request.body.decode('utf-8')
+    sVerifyMsgSig = request.GET.get('msg_signature')
+    sVerifyNonce = request.GET.get('nonce')
+    sVerifyTimeStamp = request.GET.get('timestamp')
+
+    wxcpt = getCryptor()   
+    ret,xml_content = wxcpt.DecryptMsg(encrypted_xml,sVerifyMsgSig,sVerifyTimeStamp,sVerifyNonce)
+    if ret != 0 :
+        print("ERR: VerifyURL ret: " + str(ret))
+        return HttpResponse("ERR: VerifyURL ret: " + str(ret))
+
+    type_fields = {
+        "text":["ToUserName","FromUserName","CreateTime","MsgType","Content","MsgId","AgentID"],
+        "image":["ToUserName","FromUserName","CreateTime","MsgType","PicUrl","MediaId","MsgId","AgentID"],
+        "voice":["ToUserName","FromUserName","CreateTime","MsgType","Format","MediaId","MsgId","AgentID"],
+        "video":["ToUserName","FromUserName","CreateTime","MsgType","ThumbMediaId","MediaId","MsgId","AgentID"],
+        "location":["ToUserName","FromUserName","CreateTime","MsgType","Location_X","Location_Y","Scale","Label","MsgId","AgentID"],
+        "link":["ToUserName","FromUserName","CreateTime","MsgType","Title","Description","PicUrl","MsgId","AgentID"]
+    }
+    xml_tree = ET.fromstring(xml_content)
+    type_name = xml_tree.find("MsgType").text
+    msg = {}
+    for nodename in type_fields[type_name]:
+        msg[nodename] = xml_tree.find(nodename).text
+
+    return msg
+
+def parseResponse(resp_dict):
+    wxcpt = getCryptor()
+    xml_message = ResponseMessage(resp_dict).xml
+    nonce = generateNonce()
+    ret,returnMsg = wxcpt.EncryptMsg(xml_message,nonce)
+    if ret != 0 :
+        print("ERR: VerifyURL ret: " + str(ret))
+        return HttpResponse("ERR: VerifyURL ret: " + str(ret))
+    return HttpResponse(returnMsg)
+
+
---
+++ README.md
@@ -0,0 +1,46 @@
+# BizWxCryptor
++ 企业微信自建应用加密解密类，在python3.5上测试
++ 大部分代码来自项目：https://github.com/sbzhu/weworkapi_python 
++ 修改了其不兼容python3的部分 
+
+## 文件说明
+
++ 主要是WXBizMsgCrypt.py和ierror.py这两个文件
++ BizWxUtil.py是为django2.x写的一个工具。如果用其他框架不用这个，但可以参照此文件的业务逻辑。
+
+## 依赖安装
+    
+
+    pip3 install pycryptodome
+
+> 注意有很多Crypto，安装上面这个是C大写的，并且带AES
+
+## Django示例代码
+
++ 首先在BizWxUtil.py里设置参数sToken、sEncodingAESKey、sCorpID
++ 下面是views.py中的示例代码
+
+
+
+    from django.shortcuts import render
+    from django.http import HttpResponse
+    from django.views.decorators.csrf import csrf_exempt
+    
+    from . import WorkWXUtil 
+
+    @csrf_exempt
+    def index(request):
+        # 回应url验证
+        if 'echostr' in request.GET:
+            return WorkWXUtil.responseEcho(request)
+
+        if request.method == 'POST':
+            message = WorkWXUtil.parseMessage(request)
+            # 示例，原文返回
+            text = {
+                'to_user':message['FromUserName'],
+                'from_user':message['ToUserName'],
+                'type':'text',
+                'content':message['Content'],
+            }
+            return WorkWXUtil.parseResponse(text)
